df54c7ad74fca794ee75a898cb284fb421558692
jack <jack.li@homsom.com>
2021-07-17 16:17:52 +0800

    add maven.md, modify cmd-all.md for alter iptables study, add sql/sql_table/mysql/virtualmachine.sql file

---
+++ maven.md
@@ -0,0 +1,140 @@
+#maven
+<pre>
+安装：
+wget http://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar xf apache-maven-3.3.9-bin.tar.gz -C /usr/local
+ln -sv /usr/localapache-maven-3.3.9 /usr/localmaven
+[root@BuildImage ~]# cat /etc/profile.d/maven.sh
+export PATH=$PATH:/usr/local/maven/bin/
+source /etc/profile
+
+#nexus私服介绍
+这里有很多仓库，简单介绍下
+central--中央仓库，默认从https://repo1.maven.org/maven2/拉取jar。类型：proxy
+releases --自定义的jar存储的仓库。类型：hosted
+snapshots-- 私库快照（调试版本）jar。类型：hosted
+public：仓库分组，把上面三个仓库组合在一起对外提供服务，在本地maven基础配置settings.xml中使用。类型：group
+--类型
+Nexus的仓库分为这么几类：
+hosted 宿主仓库：主要用于部署无法从公共仓库获取的构件（如 oracle 的 JDBC 驱动）以及自己或第三方的项目构件；
+proxy 代理仓库：代理公共的远程仓库；
+virtual 虚拟仓库：用于适配 Maven 1；
+group 仓库组：Nexus 通过仓库组的概念统一管理多个仓库，这样我们在项目中直接请求仓库组即可请求到仓库组管理的多个仓库。
+
+#增加maven仓库
+现在默认是从中央仓库拉取jar,我们把他改为aliyun的，岂不是更快。
+增加一个maven仓库：maven-proxy,URL指向阿里云地址：http://maven.aliyun.com/nexus/content/groups/public
+#调整maven仓库优先级
+点击maven-public 往下拉--把新建的maven-proxy仓库添加到maven-public中,并把maven-public移到最上，越上面优先级越高，当资源包被第一个仓库匹配到好就不会再去找第二个仓库了。
+我们当前仓库有三个：maven-proxy,maven-releases,maven-snapshots,将这三个仓库加入到maven-public组中即可。
+#添加用户组
+在Nexus 中创建一个javaRole的角色,拥有的权利为【nx-repository-view-maven2-*-edit】和【nx-repository-view-maven2-*-add】权利，如果该角色将来可能还有nuget,npm相关上传权利，则将其权利改为【nx-repository-view-*-*-edit】和【nx-repository-view-*-*-add】权利。
+#添加用户
+创建用户java，java用户拥有的角色为【nx-anonymous】和刚创建的【javaRole】角色。其中nx-anonymous角色是nexus默认自带的角色
+
+
+#maven settings配置文件更改：
+<!-- 配置本地仓库目录 -->
+ 	<localRepository>/data/mavenrepo</localRepository>
+<!-- 配置nexus指定仓库ID访问帐号和密码，用于命令行上传jar包，也可以使用nexus界面进行上传 -->
+    <server>
+      <id>maven-releases</id>
+      <username>java</username>
+      <password>homsom</password>
+    </server>
+
+    <server>
+      <id>maven-snapshots</id>
+      <username>java</username>
+      <password>homsom</password>
+    </server>
+<!-- 配置镜像仓库，为自己部署的私服 -->
+    <mirror>
+      <id>mirrorHomsom</id>
+      <mirrorOf>*</mirrorOf>
+      <name>NexusHomsom</name>
+      <url>http://nexus.hs.com/repository/maven-public/</url>
+    </mirror>
+<!-- 添加私服，包括仓库信息、插件仓库信息 -->
+    <profile>
+      <id>mirrorHomsom</id>
+      <repositories>
+        <repository>
+          	<id>mirrorHomsom</id>
+          	<name>mirrorHomsom</name>
+         	<url>http://nexus.hs.com/repository/maven-public/</url>
+	 	<releases>
+	  		<enabled>true</enabled>
+	  	</releases>
+	 	<snapshots>
+			<enabled>true</enabled>
+          	</snapshots>
+          	<layout>default</layout>
+          	<snapshotPolicy>always</snapshotPolicy>
+        </repository>
+      </repositories>
+
+	  <pluginRepositories>
+		<pluginRepository>
+		<id>mirrorHomsom</id>
+		<name>mirrorHomsom</name>
+		<url>http://nexus.hs.com/repository/maven-public/</url>
+		<releases>
+			<enabled>true</enabled>
+		</releases>
+		<snapshots>
+			<enabled>true</enabled>
+		</snapshots>
+		</pluginRepository>
+	  </pluginRepositories>
+    </profile>
+<!-- 开启私服，为上面的mirrorHomsom -->
+  <activeProfiles>
+    <activeProfile>mirrorHomsom</activeProfile>
+  </activeProfiles>
+
+#下载缓存基础包
+[root@BuildImage ~]# mvn help:system
+
+#命令行上传包命令
+在maven工程项目nexus-upload中的pom文件中加入
+----------------
+<project>
+ <repository>
+     <!--这里的id需要和settings.xml中的server的id一致-->
+     <id>maven-releases</id>
+     <name>Nexus release Repository</name>
+     <!--releases仓库-->
+     <url>http://nexus.hs.com/repository/maven-releases/</url>
+ </repository>
+ <snapshotRepository>
+     <id>maven-snapshots</id>
+     <name>Nexus snapshots Repository</name>
+     <!--snapshots仓库-->
+     <url>http://nexus.hs.com/repository/maven-snapshots/</url>
+ </snapshotRepository>
+</distributionManagement>
+
+</project>
+----------------
+
+
+mvn deploy:deploy-file \
+-DgroupId=com.homsom \
+-DartifactId=user-approve \
+-Dversion=0.0.1 \
+-Dpackaging=jar \
+-Dfile=user-approve-0.0.1-SNAPSHOT.jar \
+-Durl=http://nexus.hs.com/repository/maven-releases \
+-DrepositoryId=maven-releases
+注：
+groupId：目录
+artifactId：文件
+version：版本
+packaging：包类型
+repositoryId：为之前server节点中的属性id的值,--settings指定maven的setting文件
+url：nexus仓库的地址
+例如：http://nexus.hs.com/repository/maven-public/com/zzuhai/approve/0.0.1/approve-0.0.1.jar
+
+
+</pre>
---
+++ sql/sql_table/mysql/virtualmachine.sql
@@ -0,0 +1,73 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : opsmysql
+ Source Server Type    : MySQL
+ Source Server Version : 50731
+ Source Host           : mysql.hs.com:3306
+ Source Schema         : ops
+
+ Target Server Type    : MySQL
+ Target Server Version : 50731
+ File Encoding         : 65001
+
+ Date: 15/07/2021 14:18:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for virtualmachine
+-- ----------------------------
+DROP TABLE IF EXISTS `virtualmachine`;
+CREATE TABLE `virtualmachine`  (
+  `Id` int(10) NOT NULL AUTO_INCREMENT,
+  `MachineName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '机器名称',
+  `MachineModel` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '机器型号',
+  `ServiceNumber` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '服务编号',
+  `QuickServiceCode` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '快速服务代码',
+  `Rack` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '所属机柜',
+  `ManagerPort` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '管理端口',
+  `ManagerIP` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '管理IP',
+  `ManagerIPVlan` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '管理IP所属VLAN',
+  `ManagerUser` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '管理用户',
+  `ManagerPassword` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '管理密码',
+  `RAIDType` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '陈列类型',
+  `DiskType` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘类型',
+  `DiskFormat` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘规格',
+  `DiskBusType` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘总线类型',
+  `DiskCapacity` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘容量',
+  `DiskNumber` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘数量',
+  `DiskTransferSpeed` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘传输速率',
+  `DiskManufacturer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '硬盘厂商',
+  `IDRACVersion` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'IDRAC版本',
+  `MemoryModel` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内存型号',
+  `MemoryFrequency` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内存频率',
+  `MemorySize` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '内存大小',
+  `MemoryNumber` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '内存数量',
+  `CPUModel` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'CPU型号',
+  `CPUNumber` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'CPU颗数',
+  `updatetime` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of virtualmachine
+-- ----------------------------
+INSERT INTO `virtualmachine` VALUES (2, 'XEN05', 'R710', '8HNHD3X', '18481624989', 'RACK6', 'Port1', '192.168.0.201', '30', 'root', 'calvin', 'RAID-5', 'HDD', '3.5', 'SAS', '300GB', '4', '6Gbps', 'SEAGATE', '6', 'DDR-3', '1333MHz', '16G', '4', 'Xeon X5650', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (4, 'XEN08', 'R720', '3BQ3G42', '7239305810', 'RACK6', 'Port1', '192.168.0.202', '30', 'root', 'calvin', 'RAID-5', 'HDD', '3.5', 'SAS', '600GB', '6+1', '6Gbps', 'SEAGATE', '7', 'DDR-3', '1600MHz', '16G', '4', 'Xeon E5-2650 ', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (6, 'XEN03', 'R720', 'F15PMZ1', '32721795469', 'RACK6', 'Port1', '192.168.0.203', '30', 'root', 'calvin', 'RAID-10', 'HDD', '2.5/3.5', 'SAS', '600GB', '6', '6Gbps', 'SEAGATE&HITACH', '7', 'DDR-3', '1333MHz', '16G+8G', '4+2', 'Xeon E5-2650 ', '1', '2021-07-15 14:16:53');
+INSERT INTO `virtualmachine` VALUES (8, 'XEN02', 'R720', '415PMZ1', '8777189773', 'RACK5', 'Port1', '192.168.0.204', '30', 'root', 'calvin', 'RAID-50', 'HDD', '2.5/3.5', 'SAS', '600GB', '6', '6Gbps', 'SEAGATE&HITACH', '7', 'DDR-3', '1333MHz', '4G', '12', 'Xeon E5-2650 ', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (10, 'XEN06', 'R720', 'DS65F22', '30001553786', 'RACK6', 'Port1', '192.168.0.205', '30', 'root', 'calvin', 'RAID-1&RAID-5', 'HDD', '3.5', 'SAS', '146GB&600GB', '2&4', '3Gbps&6Gbps', 'SEAGATE&HITACH', '7', 'DDR-3', '1333MHz', '16G', '4', 'Xeon E5-2650 ', '1', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (12, 'XEN09', 'R730', 'H3G52M2', '37213808762', 'RACK6', 'PortIDRAC', '192.168.0.206', '30', 'root', 'calvin', 'RAID-5', 'HDD', '3.5', 'SAS', '2000GB', '8', '6Gbps', 'ATA', '8', 'DDR-4', '2133MHz', '16G', '4', 'Xeon E5-2620 ', '1', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (14, 'SQLSERVER107', 'R720', 'D0W8Q52', '28352325206', 'RACK5', 'Port1', '192.168.0.207', '30', 'root', 'calvin', 'RAID-10', 'HDD', '3.5', 'SAS', '600GB&300GB', '4', '6Gbps', 'SEAGATE&TOSHIBA', '7', 'DDR-3', '1600MHz', '16G', '4', 'Xeon E5-2630 ', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (16, 'SQLSERVER108', 'R720', 'C6G8Q52', '26511466070', 'RACK5', 'Port1', '192.168.0.208', '30', 'root', 'calvin', 'RAID-10', 'HDD', '3.5', 'SAS', '300GB', '4+1', '6Gbps', 'SEAGATE', '7', 'DDR-3', '1600MHz', '16G', '4', 'Xeon E5-2630 ', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (18, 'XEN07', 'R730', 'CJX5YK2', '27325950770', 'RACK5', 'PortIDRAC', '192.168.0.209', '30', 'root', 'calvin', 'RAID-10', 'HDD', '2.5', 'SAS', '300GB', '8', '12Gbps', 'SEAGATE', '8', 'DDR-4', '2133MHz', '16G', '4', 'Xeon E5-2680 ', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (20, 'XEN10', 'R740', '1R3JJ23', '3815319099', 'RACK5', 'PortIDRAC', '192.168.0.210', '30', 'root', 'homsom@calvin', 'RAID-5', 'SSD', '2.5', 'SATA', '480GB', '5', '6Gbps', 'MICRON', '9', 'DDR-4', '2400MHz', '16G', '4', 'Xeon Silver-4100', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (22, '192.168.13.229', 'R440', '1GYHYW2', '3202186466', 'RACK5', 'PortIDRAC', '192.168.0.211', '30', 'root', 'homsom@calvin', 'RAID-10', 'HDD', '2.5', 'SAS', '1200GB', '4', '12Gbps', 'HGST', '9', 'DDR-4', '2133MHz', '8G', '2', 'Xeon Bronze-3106', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (24, '192.168.13.228', 'R440', 'GCC1ST2', '35574350870', 'RACK5', 'PortIDRAC', '192.168.0.212', '30', 'root', 'homsom@calvin', 'RAID-1', 'SSD', '2.5', 'SATA', '480GB', '2', '6Gbps', 'ATA', '9', 'DDR-4', '2133MHz', '8G', '2', 'Xeon Bronze-3104', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (26, '192.168.13.160', 'R440', '8LSG5T2', '18731831654', 'RACK5', 'PortIDRAC', '192.168.0.213', '30', 'root', 'homsom@calvin', 'RAID-1', 'SSD', '2.5', 'SATA', '480GB', '2', '6Gbps', 'ATA', '9', 'DDR-4', '2133MHz', '8G', '2', 'Xeon Bronze-3104', '2', '2021-07-15 14:09:31');
+INSERT INTO `virtualmachine` VALUES (28, 'ESXI01', 'R740', '81RT393', '17521431735', 'RACK5', 'PortIDRAC', '192.168.0.214', '30', 'root', 'homsom@calvin', 'RAID-5', 'SSD', '2.5', 'SATA', '480GB', '5', '6Gbps', 'MICRON', '9', 'DDR-4', '2400MHz', '32G', '2', 'Xeon Silver-4210R', '2', '2021-07-15 14:09:31');
+
+SET FOREIGN_KEY_CHECKS = 1;
